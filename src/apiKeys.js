fa87f47b63fafecb64f00abe5d38f6e12db50519
Laraib Kamal Khan <khan1999laraib@gmail.com>
2023-06-26 11:55:05 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1a72641f971d8080e6bf18273a43de98",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
